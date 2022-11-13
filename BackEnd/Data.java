b5a59e43628684ac1f5d922cd6d82930d86cc0d2
Hussam <56823164+Hussam-Hassan@users.noreply.github.com>
2022-11-13 18:13:24 +0300

    Add files via upload

---
+++ BackEnd/Data.java
@@ -0,0 +1,156 @@
+
+package Data;
+public class Bean {
+
+    private String Entity1, Entity2, Entity3, Entity4, Entity5, Entity6, Entity8, Entity9, Entity10, Entity11, Entity12, Entity13, Entity14;
+    private int Entity7;
+
+    
+    public String getEntity1() {
+
+        return Entity1;
+    }
+
+    public void setEntity1(String Entity1) {
+
+
+        this.Entity1 = Entity1;
+    }
+
+    public String getEntity2() {
+
+        return Entity2;
+    }
+
+    public void setEntity2(String Entity2) {
+
+        this.Entity2 = Entity2;
+    }
+
+    public String getEntity3() {
+
+        return Entity3;
+    }
+
+    public void setEntity3(String Entity3) {
+
+        this.Entity3 = Entity3;
+    }
+
+    public String getEntity4() {
+
+        return Entity4;
+    }
+
+    public void setEntity4(String Entity4) {
+
+        this.Entity4 = Entity4;
+    }
+
+    public String getEntity5() {
+
+        return Entity5;
+    }
+
+    public void setEntity5(String Entity5) {
+
+        this.Entity5 = Entity5;
+    }
+
+    public String getEntity6() {
+
+        return Entity6;
+    }
+
+    public void setEntity6(String Entity6) {
+
+        this.Entity6 = Entity6;
+    }
+    
+    public int getEntity7() {
+
+        return Entity7;
+    }
+
+    public void setEntity7(String Entity7) {
+
+        this.Entity7 = Integer.parseInt(Entity7);
+
+    }
+    public void setEntity7(int Entity7) {
+
+        this.Entity7 = Entity7;
+    }
+
+    public String getEntity8() {
+
+        return Entity8;
+    }
+
+    public void setEntity8(String Entity8) {
+
+        this.Entity8 = Entity8;
+    }
+
+    public String getEntity9() {
+
+        return Entity9;
+    }
+
+    public void setEntity9(String Entity9) {
+
+        this.Entity9 = Entity9;
+    }
+
+    public String getEntity10() {
+
+        return Entity10;
+    }
+
+    public void setEntity10(String Entity10) {
+
+        this.Entity10 = Entity10;
+    }
+
+    public String getEntity11() {
+
+        return Entity11;
+    }
+
+    public void setEntity11(String Entity11) {
+
+        this.Entity11 = Entity11;
+    }
+
+    public String getEntity12() {
+
+        return Entity12;
+    }
+
+    public void setEntity12(String Entity12) {
+
+        this.Entity12 = Entity12;
+    }
+
+    public String getEntity13() {
+
+        return Entity13;
+    }
+
+    public void setEntity13(String Entity13) {
+
+        this.Entity13 = Entity13;
+    }
+
+    public String getEntity14() {
+
+        return Entity14;
+    }
+
+    public void setEntity14(String Entity14) {
+
+        this.Entity14 = Entity14;
+    }
+    
+    
+}
